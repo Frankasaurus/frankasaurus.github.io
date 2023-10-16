ffbb5f901ff8fd617b2aefc929b814f4839441bd
Alexander Franke <45259117+Frankasaurus@users.noreply.github.com>
2023-10-16 23:03:46 +0200

    Quartz sync: Oct 16, 2023, 11:03 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Frankasaurus' Obsidian Repo",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "frankasaurus.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
